870aecce3e31ed6be6a73b42545336e66323b59b
Jesus Davi Pontes Anhaia <98184636+TheDrack@users.noreply.github.com>
2026-02-19 01:35:22 -0300

    Criar app/application/services/structured_logger.py

---
+++ app/application/services/structured_logger.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+
+class StructuredLogger:
+    """Wrapper for structured logging with context"""
+    def __init__(self, logger_instance, **context):
+        self.logger = logger_instance
+        self.context = context
+
+    def _log(self, level, msg, **extra):
+        log_data = {**self.context, **extra, "message": msg}
+        self.logger.log(level, json.dumps(log_data))
+
+    def info(self, msg, **extra): self._log(logging.INFO, msg, **extra)
+    def error(self, msg, **extra): self._log(logging.ERROR, msg, **extra)
+    def warning(self, msg, **extra): self._log(logging.WARNING, msg, **extra)
+    def debug(self, msg, **extra): self._log(logging.DEBUG, msg, **extra)
