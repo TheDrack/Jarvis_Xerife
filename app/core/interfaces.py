52eff49f1f5ae79bf72e06636fb4ab3523b29067
Jesus Davi Pontes Anhaia <98184636+TheDrack@users.noreply.github.com>
2026-02-24 20:56:17 -0300

    Criar interfaces.py

---
+++ app/core/interfaces.py
@@ -0,0 +1,8 @@
+# app/core/interfaces.py
+from abc import ABC, abstractmethod
+
+class NexusComponent(ABC):
+    """Interface obrigatória para descoberta via JarvisNexus"""
+    @abstractmethod
+    def execute(self, *args, **kwargs):
+        pass
